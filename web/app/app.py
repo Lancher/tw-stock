a168c85e57344ca71a996bc120b4cd5403ccf1d4
Steve <steve.liushihao@gmail.com>
2017-08-10 08:28:24 -0700

    use supervisor to start all service

--- web/app/app.py
+++ web/app/app.py
@@ -2,16 +2,29 @@
 #  -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, with_statement
 
-
+# 3rd party
 import tornado.ioloop
 import tornado.web
 import tornado.options
-
+import MySQLdb
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
+
+
+class API1Handler(tornado.web.RequestHandler):
+
+    def get(self):
+        # get latest date
+        con = MySQLdb.connect(host='db', user='root', passwd='1234', db='stock')
+        with con:
+            cur = con.cursor()
+            cur.execute("SELECT latest_date FROM info ORDER BY id DESC LIMIT 1")
+            d = cur.fetchone()
+
+        self.write(d)
 
 
 def main():
@@ -21,6 +34,7 @@ def main():
     # web
     app = tornado.web.Application([
         (r"/api", MainHandler),
+        (r"/api1", API1Handler),
         (r'/(.*)', tornado.web.StaticFileHandler, {'path': './frontend', "default_filename": "index.html"})
     ])
     app.listen(80)
